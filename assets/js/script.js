8b259a18118b7ccadda5ab717718bbb812089680
SteveMiller7 <info@millercreativedesign.com>
2021-12-15 22:58:30 +0000

    link js file to html. begin to set up functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,31 @@
-let arrowButtons = document.getElementsByClassName('arrows');
+let buttons = document.getElementsByTagName('button');
 let imageBox = document.getElementById("game-container");
 let playerScore = document.getElementById("player-score");
 let goalieScore = document.getElementById("goalie-score");
-let shotChoice = ["Shoot Left", "Shoot Middle", "Shoot Right"]
+let shotChoice = ["left", "middle", "right"]
 
+
+
+
+// adding event listener to buttons
+
+for (let button of buttons){
+    button.addEventLister("click", function() {
+        let playerShotChoice = document.getAttribute("data-choice");
+        playGame(playerShotChoice);
+    });
+}
+
+
+// Game function
+
+function playGame(playerShotChoice) {
+
+    playerImage.src = `assets/images/${shotChoice[playerShotChoice]}.png`;
+    playerImage.alt = shotChoice[playerShotChoice];
+
+    let goalieChoice = Math.floor(Math.random() *3);
+
+    goalieImage.src = `assets/images/${shotChoice[goalieChoice]}.png`;
+    goalieImage.alt = shotChoice[goalieChoice];
+*/
