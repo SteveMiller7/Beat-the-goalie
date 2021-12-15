67ff0e9117bedda876f27c3cc326228444bf72f7
SteveMiller7 <info@millercreativedesign.com>
2021-12-15 19:14:03 +0000

    Start js script

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+let arrowButtons = document.getElementsByClassName('arrows');
+let imageBox = document.getElementById("game-container");
+let playerScore = document.getElementById("player-score");
+let goalieScore = document.getElementById("goalie-score");
+let shotChoice = ["Shoot Left", "Shoot Middle", "Shoot Right"]
