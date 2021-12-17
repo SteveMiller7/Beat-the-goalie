a6e791079a0c4a08e49defc7560fa2055044ed4d
SteveMiller7 <info@millercreativedesign.com>
2021-12-17 13:36:41 +0000

    remove check goal function and update score functions. Update playGame function to update score. Game now working

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,57 +22,42 @@ let goalieChoice = Math.floor(Math.random() * 3)+1;
 
     if (goalieChoice === 1 && playerShotChoice === "0") {
         mainImage.src = "assets/images/GLBL-01.png";
+        let oldScore = parseInt(document.getElementById("goalie").innerText);
+        document.getElementById("goalie").innerText = ++oldScore;
     } else if (goalieChoice === 1 && playerShotChoice === "1") {
         mainImage.src = "assets/images/GLBM-01.png";
+        let oldScore = parseInt(document.getElementById("player").innerText);
+	    document.getElementById("player").innerText = ++oldScore;
     } else if (goalieChoice === 1 && playerShotChoice === "2") {
         mainImage.src = "assets/images/GLBR-01.png";
+        let oldScore = parseInt(document.getElementById("player").innerText);
+	    document.getElementById("player").innerText = ++oldScore;
     } else if (goalieChoice === 2 && playerShotChoice === "0") {
         mainImage.src = "assets/images/GMBL-01.png";
+        let oldScore = parseInt(document.getElementById("player").innerText);
+	document.getElementById("player").innerText = ++oldScore;
     } else if (goalieChoice === 2 && playerShotChoice === "1") {
         mainImage.src = "assets/images/GMBM-01.png";
+        let oldScore = parseInt(document.getElementById("goalie").innerText);
+        document.getElementById("goalie").innerText = ++oldScore;
     } else if (goalieChoice === 2 && playerShotChoice === "2") {
         mainImage.src = "assets/images/GMBR-01.png";
+        let oldScore = parseInt(document.getElementById("player").innerText);
+	document.getElementById("player").innerText = ++oldScore;
     } else if (goalieChoice === 3 && playerShotChoice === "0") {
         mainImage.src = "assets/images/GRBL-01.png";
+        let oldScore = parseInt(document.getElementById("player").innerText);
+	document.getElementById("player").innerText = ++oldScore;
     } else if (goalieChoice === 3 && playerShotChoice === "1") {
         mainImage.src = "assets/images/GRBM-01.png";
+        let oldScore = parseInt(document.getElementById("player").innerText);
+	document.getElementById("player").innerText = ++oldScore;
     } else if (goalieChoice === 3 && playerShotChoice === "2") {
         mainImage.src = "assets/images/GRBR-01.png";
+        let oldScore = parseInt(document.getElementById("goalie").innerText);
+        document.getElementById("goalie").innerText = ++oldScore;
 
     }
     
-    //checkGoal();
 
-    //updateScores(result);
-    incrementPlayerScore()
-    incrementGoalieScore()
-}
-/*
-function checkGoal() {
-
-    let playerChoice = playerShotChoice.value;
-	let computerChoice = goalieChoice.value;
-	let isGoal = (playerChoice === computerChoice);
-
-	if (isGoal) {
-		console.log("Goalie saved the shot!");
-		incrementGoalieScore();
-	} else {
-		console.log("GOAAAAAAAL!");
-		incrementPlayerScore();
-	}
-   
-}
-*/
-
-// Increase player score with each goal scored
-function incrementPlayerScore() {
-    let oldScore = parseInt(document.getElementById("player").innerText);
-	document.getElementById("player").innerText = ++oldScore;
-}
-
-// Increase goalie score with ech save.
-function incrementGoalieScore() {
-    let oldScore = parseInt(document.getElementById("goalie").innerText);
-	document.getElementById("goalie").innerText = ++oldScore;
 }
