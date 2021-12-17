29c8d6419b4a65a6c2ee420af84214d11f040d52
SteveMiller7 <info@millercreativedesign.com>
2021-12-17 14:12:23 +0000

    Added console logs for Goal and Shot saved

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,38 +24,47 @@ let goalieChoice = Math.floor(Math.random() * 3)+1;
         mainImage.src = "assets/images/GLBL-01.png";
         let oldScore = parseInt(document.getElementById("goalie").innerText);
         document.getElementById("goalie").innerText = ++oldScore;
+        console.log("Shot saved!")
     } else if (goalieChoice === 1 && playerShotChoice === "1") {
         mainImage.src = "assets/images/GLBM-01.png";
         let oldScore = parseInt(document.getElementById("player").innerText);
 	    document.getElementById("player").innerText = ++oldScore;
+        console.log("Goaaaaal!")
     } else if (goalieChoice === 1 && playerShotChoice === "2") {
         mainImage.src = "assets/images/GLBR-01.png";
         let oldScore = parseInt(document.getElementById("player").innerText);
 	    document.getElementById("player").innerText = ++oldScore;
+        console.log("Goaaaaal!")
     } else if (goalieChoice === 2 && playerShotChoice === "0") {
         mainImage.src = "assets/images/GMBL-01.png";
         let oldScore = parseInt(document.getElementById("player").innerText);
 	document.getElementById("player").innerText = ++oldScore;
+    console.log("Goaaaaal!")
     } else if (goalieChoice === 2 && playerShotChoice === "1") {
         mainImage.src = "assets/images/GMBM-01.png";
         let oldScore = parseInt(document.getElementById("goalie").innerText);
         document.getElementById("goalie").innerText = ++oldScore;
+        console.log("Shot saved!")
     } else if (goalieChoice === 2 && playerShotChoice === "2") {
         mainImage.src = "assets/images/GMBR-01.png";
         let oldScore = parseInt(document.getElementById("player").innerText);
 	document.getElementById("player").innerText = ++oldScore;
+    console.log("Goaaaaal!")
     } else if (goalieChoice === 3 && playerShotChoice === "0") {
         mainImage.src = "assets/images/GRBL-01.png";
         let oldScore = parseInt(document.getElementById("player").innerText);
 	document.getElementById("player").innerText = ++oldScore;
+    console.log("Goaaaaal!")
     } else if (goalieChoice === 3 && playerShotChoice === "1") {
         mainImage.src = "assets/images/GRBM-01.png";
         let oldScore = parseInt(document.getElementById("player").innerText);
 	document.getElementById("player").innerText = ++oldScore;
+    console.log("Goaaaaal!")
     } else if (goalieChoice === 3 && playerShotChoice === "2") {
         mainImage.src = "assets/images/GRBR-01.png";
         let oldScore = parseInt(document.getElementById("goalie").innerText);
         document.getElementById("goalie").innerText = ++oldScore;
+        console.log("Shot saved!")
 
     }
     
