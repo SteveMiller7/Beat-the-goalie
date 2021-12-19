58a7b4d22b17ee221bf5e6b21943226d599346ca
SteveMiller7 <info@millercreativedesign.com>
2021-12-19 12:36:05 +0000

    add win and lose function. after hours and hours of trying. beer time

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,111 +22,80 @@ function playGame(playerShotChoice) {
 
     if (goalieChoice === 1 && playerShotChoice === "0") {
         mainImage.src = "assets/images/GLBL-01.png";
-      //  let oldGScore = parseInt(document.getElementById("goalie").innerText);
-       // document.getElementById("goalie").innerText = ++oldGScore;
-       incrementGScore()
+        //  let oldGScore = parseInt(document.getElementById("goalie").innerText);
+        // document.getElementById("goalie").innerText = ++oldGScore;
+        incrementGScore()
 
     } else if (goalieChoice === 1 && playerShotChoice === "1") {
         mainImage.src = "assets/images/GLBM-01.png";
-      //  let oldPScore = parseInt(document.getElementById("player").innerText);
-      //  document.getElementById("player").innerText = ++oldPScore;
-      incrementPScore()
+        //  let oldPScore = parseInt(document.getElementById("player").innerText);
+        //  document.getElementById("player").innerText = ++oldPScore;
+        incrementPScore()
     } else if (goalieChoice === 1 && playerShotChoice === "2") {
         mainImage.src = "assets/images/GLBR-01.png";
-       // let oldPScore = parseInt(document.getElementById("player").innerText);
-       // document.getElementById("player").innerText = ++oldPScore;
-       incrementPScore()
+        // let oldPScore = parseInt(document.getElementById("player").innerText);
+        // document.getElementById("player").innerText = ++oldPScore;
+        incrementPScore()
     } else if (goalieChoice === 2 && playerShotChoice === "0") {
         mainImage.src = "assets/images/GMBL-01.png";
-       // let oldPScore = parseInt(document.getElementById("player").innerText);
-       // document.getElementById("player").innerText = ++oldPScore;
-       incrementPScore()
+        // let oldPScore = parseInt(document.getElementById("player").innerText);
+        // document.getElementById("player").innerText = ++oldPScore;
+        incrementPScore()
     } else if (goalieChoice === 2 && playerShotChoice === "1") {
         mainImage.src = "assets/images/GMBM-01.png";
-       // let oldGScore = parseInt(document.getElementById("goalie").innerText);
-       // document.getElementById("goalie").innerText = ++oldGScore;
-       incrementGScore()
+        // let oldGScore = parseInt(document.getElementById("goalie").innerText);
+        // document.getElementById("goalie").innerText = ++oldGScore;
+        incrementGScore()
     } else if (goalieChoice === 2 && playerShotChoice === "2") {
         mainImage.src = "assets/images/GMBR-01.png";
-       // let oldPScore = parseInt(document.getElementById("player").innerText);
-       // document.getElementById("player").innerText = ++oldPScore;
-       incrementPScore()
+        // let oldPScore = parseInt(document.getElementById("player").innerText);
+        // document.getElementById("player").innerText = ++oldPScore;
+        incrementPScore()
     } else if (goalieChoice === 3 && playerShotChoice === "0") {
         mainImage.src = "assets/images/GRBL-01.png";
-       // let oldPScore = parseInt(document.getElementById("player").innerText);
-       // document.getElementById("player").innerText = ++oldPScore;
-       incrementPScore()
+        // let oldPScore = parseInt(document.getElementById("player").innerText);
+        // document.getElementById("player").innerText = ++oldPScore;
+        incrementPScore()
     } else if (goalieChoice === 3 && playerShotChoice === "1") {
         mainImage.src = "assets/images/GRBM-01.png";
-       // let oldPScore = parseInt(document.getElementById("player").innerText);
-       // document.getElementById("player").innerText = ++oldPScore;
-       incrementPScore()
+        // let oldPScore = parseInt(document.getElementById("player").innerText);
+        // document.getElementById("player").innerText = ++oldPScore;
+        incrementPScore()
     } else if (goalieChoice === 3 && playerShotChoice === "2") {
         mainImage.src = "assets/images/GRBR-01.png";
         //let oldGScore = parseInt(document.getElementById("goalie").innerText);
-       // document.getElementById("goalie").innerText = ++oldGScore;
-       incrementGScore()
+        // document.getElementById("goalie").innerText = ++oldGScore;
+        incrementGScore()
     }
 
 
 }
 
 function incrementPScore() {
-
-
-	let oldPScore = parseInt(document.getElementById("player").innerText);
-        document.getElementById("player").innerText = ++oldPScore;
-
+    let oldPScore = parseInt(document.getElementById("player").innerText);
+    document.getElementById("player").innerText = ++oldPScore;
+    gameOver();
 }
+
+
 
 function incrementGScore() {
-
-
-	let oldGScore = parseInt(document.getElementById("goalie").innerText);
-        document.getElementById("goalie").innerText = ++oldGScore;
-
+    let oldGScore = parseInt(document.getElementById("goalie").innerText);
+    document.getElementById("goalie").innerText = ++oldGScore;
+    gameOver();
 }
 
-/*
-let gScore = parseInt(document.getElementById("goalie").innerText);
-let pScore = parseInt(document.getElementById("player").innerText);
-
-function reset() { pScore = 0; gScore = 0; }
-
-function endGame() {
-    if (pScore == 2 || gScore == 2) {
-        reset();
-    }
-}*/
-/*
 function gameOver() {
     let gScore = parseInt(document.getElementById("goalie").innerText);
     let pScore = parseInt(document.getElementById("player").innerText);
 
-    if (+pScore == 2) {
+    if (pScore == 2) {
         mainImage.src = "assets/images/WIN-01.png";
         reset;
 
-    } else if (+gScore == 2) {
+    } else if (gScore == 2) {
         mainImage.src = "assets/images/LOSE-01.png";
         reset;
     }
-}*/
+}
 
-/*let pScore = parseInt(document.getElementById("player").innerText)
-
-function playerWin()
-{
-    if (pScore >= "10") {
-        mainImage.src = "assets/images/WIN-01.png";
-        return (playGame);
-}}
-
-let gScore = parseInt(document.getElementById("goalie").innerText)
-
-function goalieWin()
-{
-    if (gScore === "10") {
-        mainImage.src = "assets/images/LOSE-01.png";
-        return (playGame);
-}}*/
